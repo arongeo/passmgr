fb986f20643b4754fbe060451cc30c941acc532f
mrmalac <58132897+mrmalac@users.noreply.github.com>
2021-09-10 18:45:19 +0200

    Updated tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ import auth
 
 class Testing(unittest.TestCase):
     def test_hash(self):
-        self.assertEqual(crypt.hash("test"), "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
+        self.assertEqual(crypt.hash("test"), "3a7b0c3880ac54833abda5957dad646380a312a5b985adbe23e33a2ba6a10a3e")
 
 
 if __name__ == '__main__':
