bdba4db6c1d4d7ae13c84470fa5ddd5d216ea489
mrmalac <58132897+mrmalac@users.noreply.github.com>
2021-09-10 18:37:50 +0200

    Added things

--- tests.py
+++ tests.py
@@ -1,9 +1,14 @@
+import unittest
 import app
 import database
 import crypt
 import menu
 import auth
 
-def test_hash():
-    assert "test" != crypt.hash("test")
+class Testing(unittest.TestCase):
+    def test_hash(self):
+        self.assertEqual(crypt.hash("test"), "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08")
 
+
+if __name__ == '__main__':
+    unittest.main()
